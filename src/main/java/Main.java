7b17b5e5a1fc5896e2897545d1971c8628eada5c
mateus01gk <169069087+mateus01gk@users.noreply.github.com>
2024-05-14 00:12:56 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,29 @@
+
+
+
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");    
+  System.out.println("  ");
+     System.out.println("!!!!!!!!!!");
+     System.out.println("! Relato !");
+     System.out.println("!!!!!!!!!!");
+     System.out.println("  ");
+     System.out.println("Me chamo Mateus, estou no primeiro ano do Ensino Médio e estou cursando Desenvolvimento e Análise de Sistemas.Uma das disciplinas que estou estudando é lógica computacional, e gostaria de falar um pouco sobre isso.");
+     System.out.println("  ");
+    System.out.println("No início do ano, nós começamos a explorar tabelas de verdade e tivemos muitos exercícios para fazer, o que foi ótimo para entender melhor o assunto. No final do primeiro trimestre, começamos a ver algoritmos, o que foi super útil para entender como começar a programar.");
+       System.out.println("  ");
+System.out.println("Agora, no início do segundo trimestre, estamos nos aprofundando na programação. Estamos aprendendo a escrever códigos de forma rápida e eficiente, o que está ajudando muito no nosso aprendizado.");
+     System.out.println("  ");
+  System.out.println("Estou curtindo bastante o curso e sinto que estou evoluindo cada vez mais na arte da programação.");
+     System.out.println("  ");
+     System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+
+  }
+
+}
+
+
+
+
